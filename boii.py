233020eac4c9993b474d7d01216792a28f6a98e2
Shimakai <45695631+Shimakai@users.noreply.github.com>
2020-08-09 23:22:18 +0200

    Update boii.py

--- boii.py
+++ boii.py
@@ -2,7 +2,7 @@
 si_no = ["si", "no"]
 direzioni = ["sinistra", "destra", "avanti", "indietro"]
 #introduzione
-nome =input("Benvenuto nel sistema RAGNAROK, un mondo privo di alcuna legge terrena ove avvengono scontri tra due fazioni ...... e ...... Il tuo obbiettivo sarà lasciare segno nella storia facendo si che chiunque possa rimembrare le tue gesta, perciò scrivi il nome col quale vorrai essere ricordato:")
+nome =input("Benvenuto nel sistema RAGNAROK, un mondo privo di alcuna legge terrena ove avvengono scontri tra due fazioni ...... e ...... Il tuo obbiettivo sarà lasciare segno nella storia facendo si che chiunque possa rimembrare le tue gesta, perciò scrivi il nome col quale vorrai essere ricordato:\n")
 print("Mhhh,"+ nome + " quindi? \n Ottimo, che le tue gesta possano essere tramandate per l'eternità")
 #si_inizia_boiiiiiiiiiiiii
 risposta = ""
